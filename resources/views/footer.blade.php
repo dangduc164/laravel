8a4b3f5d937bb41419998ac4c1d12dd8def674af
DangDuc <ducden164@gmail.com>
2022-08-02 20:49:35 +0700

    update footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,24 +3,8 @@
         <div class="container">
             <div class="footer_logo">
                 <h1>
-                    <a href="index.html"><b>ĐQ.SHOP</b></a>
+                    <a href="{{ route('welcome') }}"><b>ĐQ.SHOP</b></a>
                 </h1>
-            </div>
-            <div class="input_bt">
-                <input type="text" class="mail_bt" placeholder="Your Email" name="Your Email" />
-                <span class="subscribe_bt" id="basic-addon2"><a href="#">Subscribe</a></span>
-            </div>
-            <div class="footer_menu">
-                <ul>
-                    <li><a href="#">Bán Chạy nhất</a></li>
-                    <li><a href="#">Quà Tặng</a></li>
-                    <li><a href="#">Hàng Mới </a></li>
-                    <li><a href="#">Ưu đã hôm nay</a></li>
-                </ul>
-            </div>
-            <div class="location_main">
-                <img src="./images/call-icon.png" alt="call">
-                Hot Line: <a href="#">+84967771606</a>
             </div>
         </div>
     </div>
